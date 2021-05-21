8f63eceb5899dd3df0736298aeb7f653cd8d3d1c
Moinak Banerjee <moinak878@gmail.com>
2021-05-21 18:28:34 +0530

    :rocket: learning about variables

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # AlonGoLang
 Learning Go(GoLang) :eyes:
 
-# Lesson 1
-Writing the basic hello world program in go.
+# Lesson 2
+Learning about variable types in Go 
+
+# Quote
+In a world full of variables, be my constant... :penguin: :penguin:
+
 
 ## Key points ....
-
-1. Learn more about fmt 
-2. fmt.Println -> uppercase P coz exports from (fmt) package always in upper case 
-3. only one main() can be there 
-4. package is main signifies standalone application 
+1. string always in double quotes
+2. shorthand variable notation without specifying var keyword or datatype
+3. int8, int16, uint, etc. ( whats the default of int ? )
+4. float32 & float64 
+5. default of float is float64
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
+// In a world full of variables , be my constant
 package main
 
 import "fmt"
+// declaring variables and not using them is an error in go XD
 
 func main(){
-	fmt.Println("Hello, world")
+
+	// =============================String=================================
+
+	var name string ="moinak"
+	var name2 = "noob" // go automatically infers the type of variable 
+	var name3 string // defualt value -> empty string 
+
+	fmt.Println(name)
+	fmt.Println(name2,name3)
+
+	name="moinakv2.0"
+	name3="piro"
+
+	fmt.Println(name,name2,name3) // prints space separated values on screen
+
+	
+	name4 := "woah" // shorthand way of declaring variable
+	fmt.Println(name4)
+
+	// ====================================================================
+
+
+	
+	// =============================Integer=================================
+	
+	var age1 int = 20
+	var age2 = 30
+	age3 := 40
+
+	fmt.Println(age1,age2,age3)
+	// bits and memory
+	// var age4 int8 = 215 //error : out of range
+	// var age5 uint =123 //only positive numbers 
+
+	// ====================================================================
+
+
+
+	// =======================float========================================
+
+	var scoreOne float32 = 15.7  // must specify whether 32 or 64 
+	scoreTwo  := 13242341234134235.7 // float64 by default
+	fmt.Println(scoreOne,scoreTwo)
+
 }
